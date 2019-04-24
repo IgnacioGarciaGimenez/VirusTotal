b236de2b86a8df236f4bfb0f1ba34ad0d1e1ab0a
Maximiliano Pomar <maxipomar@gmail.com>
2019-04-24 19:19:45 -0300

    Logre la conexion con el servidor, aunque ahora tira un error de COR

--- Cliente/src/app/services/file.services.ts
+++ Cliente/src/app/services/file.services.ts
@@ -1,9 +1,7 @@
 import {Injectable, Inject} from '@angular/core';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
+
 
 
 @Injectable()
@@ -15,14 +13,32 @@ export class FileServices {
     this.myAppUrl = baseUrl;
   }
 
-  analyzeFile(file) {
-    return this.http.get(this.myAppUrl + 'api/User').subscribe(
+  uploadFile(file) {
+    const hash = '42e449fbbbe98aba4c79d1434d0b11923379faba943e888e0966766379dcdf28';
+    /*    let params = new HttpParams();
+        params = params.append('hash', hash);*/
+    return this.http.get(this.myAppUrl + 'api/VirusTotal/' + hash).subscribe(
       (data) => {
         // some code
         console.log('The file was analyze');
       },
       (error1 => {
-        console.error('An error ocurred trying to get the file info')
+        console.error('An error ocurred trying to get the file info');
+        throwError(error1);
+      })
+    );
+  }
+  uploadFileToInternet(file) {
+    const hash = '42e449fbbbe98aba4c79d1434d0b11923379faba943e888e0966766379dcdf28';
+    /*    let params = new HttpParams();
+        params = params.append('hash', hash);*/
+    return this.http.get(this.myAppUrl + 'ui/files/' + hash).subscribe(
+      (data) => {
+        // some code
+        console.log('The file was analyze');
+      },
+      (error1 => {
+        console.error('An error ocurred trying to get the file info');
         throwError(error1);
       })
     );
--- Cliente/src/main.ts
+++ Cliente/src/main.ts
@@ -1,12 +1,23 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+
+export function getBaseUrl() {
+  // const baseUrlmocked = 'https://www.virustotal.com/';
+  const baseUrlmocked = 'http://localhost:59905/';
+  return baseUrlmocked;
+  // return document.getElementsByTagName('base')[0].href;
+}
+
+const providers = [
+  {provide: 'BASE_URL', useFactory: getBaseUrl, deps: []}
+];
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
+  .catch(err => console.log(err));
